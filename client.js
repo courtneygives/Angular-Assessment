8642e50aea111354f74b7bbc1bebf3a1f2620ef2
Courtney Ives <cgives@me.com>
2016-04-22 16:59:37 -0500

    end of assessment time

--- client.js
+++ client.js
@@ -1,16 +1,30 @@
-var app = angular.module('assessment');
+var app = angular.module('assessment', []);
 
+// :::::: START CONTROLLER :::::: //
 app.controller('BehindCurtain', [function(){
   var vm = this;
-  vm.message = {};
+  vm.message = '';
   vm.list = [];
+  vm.listShow = false;
 
-  vm.getList = function (){
+vm.addMsg = function(input){
+  vm.listShow = true;
+  vm.list.push(vm.message);
+  vm.message = '';
+};
 
+  vm.clear = function (){
+    vm.message = '';
+  };
+
+  vm.emptyList = function (){
+    vm.list = [];
+    vm.listShow = false;
   };
 
 
 
 
 
+// :::::: CLOSE CONTROLLER :::::: //
 }]);
