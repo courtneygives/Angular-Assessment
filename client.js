a5dc799e511f717ab536ec610bf6095ec1f0c787
Courtney Ives <cgives@me.com>
2016-04-22 13:22:03 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,16 @@
+var app = angular.module('assessment');
+
+app.controller('BehindCurtain', [function(){
+  var vm = this;
+  vm.message = {};
+  vm.list = [];
+
+  vm.getList = function (){
+
+  };
+
+
+
+
+
+}]);
